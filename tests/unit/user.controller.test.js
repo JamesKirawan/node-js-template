c4e92d6750a1f28aa6c2453600b104cedc31eb7c
James Kirawan <jameskirawan@gmail.com>
2022-05-17 20:27:09 +0700

    tidy up unit testing

--- tests/unit/user.controller.test.js
+++ tests/unit/user.controller.test.js
@@ -30,6 +30,14 @@ const testingData = {
   password: hashPassword("secret"),
 };
 
+const testingData2 = {
+  id: 1,
+  firstName: "Jems",
+  lastName: "Kirawan",
+  email: "asd@gmail.com",
+  password: hashPassword("polisi"),
+};
+
 describe("UserController.registerUser", () => {
   it("should return 201", async () => {
     User.create.mockResolvedValue(userData);
@@ -60,5 +68,32 @@ describe("UserController.registerUser", () => {
     req.body = userData;
     await UserController.registerUser(req, res);
     expect(res.statusCode).toBe(500);
+  });
+});
+
+describe("UserController.loginUser", () => {
+  it("should return 200", async () => {
+    User.findOne.mockResolvedValue(testingData);
+    req.body = userData;
+    await UserController.loginUser(req, res);
+    expect(res.statusCode).toBe(200);
+  });
+  it("should return 400", async () => {
+    User.findOne.mockResolvedValue(null);
+    await UserController.loginUser(req, res);
+    expect(res.statusCode).toBe(400);
+  });
+  it("should return 400", async () => {
+    User.findOne.mockResolvedValue(testingData);
+    req.body = testingData2;
+    await UserController.loginUser(req, res);
+    expect(res.statusCode).toBe(400);
+  });
+  it("should return 401", async () => {
+    const rejected = Promise.reject({ message: "Error" });
+    User.findOne.mockResolvedValue(rejected);
+    req.body = userData;
+    await UserController.loginUser(req, res);
+    expect(res.statusCode).toBe(401);
   });
 });
